d0cad16e9b7b4bfb3edaac59cc71dba4a5b715ad
morinoko <forby.1@gmail.com>
2013-09-05 23:17:41 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fuwafuwa::Application.config.secret_key_base = 'dde242dd7e8f2626cd611c0c81834499028774e5e7c58c32aa330a3d01066f59f10b6ce11810cabcca54b446adf9d454d0e0af44eaeaaa6f38bee4ae6b4308ff'
